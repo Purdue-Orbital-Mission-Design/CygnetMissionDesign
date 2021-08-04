adb4a133df82a0f9521f8d42a7e51f166a042624
liam <robin502@purdue.edu>
2021-08-04 00:36:20 -0400

    everything is still broken

---
+++ getBrightnessOfFigure.m
@@ -0,0 +1,16 @@
+function [brightness, image] = getBrightnessOfFigure(ax1, fig2)
+   delete(gca)
+   ax2 = copyobj(ax1, fig2);
+
+   set(fig2, 'GraphicsSmoothing', 'off')
+   set(ax2, 'Color', 'k', 'xtick', [], 'ytick', [], 'ztick', [])
+   
+   frame = getframe();
+   colimage = frame2im(frame);
+   image = rgb2gray(colimage);
+   
+   image(image == 240 | image == 210 | image == 255) = 0;
+   
+   
+   brightness = sum(sum(image)) / numel(image(image ~= 0)) / 255;
+end
--- test_intersct.m
+++ test_intersct.m
@@ -8,10 +8,6 @@ objFile = 'OBJ_for_testing/deer_triangulated.obj';
 current_patch = RenderObj(objFile, C);
 hold on
 
-rotx = @(t) [1 0 0; 0 cos(t) -sin(t) ; 0 sin(t) cos(t)];
-roty = @(t) [cos(t) 0 sin(t) ; 0 1 0 ; -sin(t) 0  cos(t)];
-rotz = @(t) [cos(t) -sin(t) 0 ; sin(t) cos(t) 0 ; 0 0 1];
-
 tic
 ordered_triads = zeros(length(current_patch.Faces) * 3, 3);
 
@@ -19,15 +15,20 @@ for i = 1:length(current_patch.Faces)
     ordered_triads(i*3-2:i*3, :) = current_patch.Vertices(current_patch.Faces(i, :), :);
 end
 
-incoming_vector = [1; 0; 0];
-planebasis1 = roty(pi / 2) * incoming_vector;
-planebasis2 = cross(planebasis1, incoming_vector);
-flattened_ordered_coords = ordered_triads * [planebasis2, planebasis1];
+incoming_vector = [1; 1; 0];
+incoming_vector = incoming_vector / norm(incoming_vector);
+
+point_in_projection_plane = null(incoming_vector')*((rand(2, 1)-0.5)*2); % <- here are the coordinates of random points
+
+planebasis1 = point_in_projection_plane / norm(point_in_projection_plane);
+planebasis2 = cross(incoming_vector, planebasis1);
+
+flattened_ordered_triads = ordered_triads * [planebasis1, planebasis2];
 
 apparent_facet_areas = zeros(length(current_patch.Faces), 1);
 for i = 1:length(current_patch.Faces)
-    x_vals = flattened_ordered_coords(i*3-2:i*3-0, 1);
-    y_vals = flattened_ordered_coords(i*3-2:i*3-0, 2);
+    x_vals = flattened_ordered_triads(i*3-2:i*3-0, 1);
+    y_vals = flattened_ordered_triads(i*3-2:i*3-0, 2);
     apparent_facet_areas(i) = 1 / 2 * det([x_vals, y_vals, [1; 1; 1]]);
     
     if apparent_facet_areas(i) < epsilon
@@ -35,20 +36,16 @@ for i = 1:length(current_patch.Faces)
     end
 end
 
-flattened_triangles = polyshape.empty;
-for i = 1:length(current_patch.Faces)
-    if ~ismember(i, skip_faces)
-        flattened_triangles(i) = polyshape(flattened_ordered_coords(i*3-2:i*3-0, 1), flattened_ordered_coords(i*3-2:i*3-0, 2));
-    end
-end
-
 facet_normals = zeros(length(current_patch.Faces), 3);
 facet_centroids = zeros(length(current_patch.Faces), 3);
+flattened_facet_centroids = zeros(length(current_patch.Faces), 2);
 facet_normals_to_incoming = zeros(length(current_patch.Faces), 1);
 facet_force_contributions = zeros(length(current_patch.Faces), 1);
 
+facet_centroid_to_projection_plane = zeros(length(current_patch.Faces), 3);
+
 for i = 1:length(current_patch.Faces)
-    if ~ismember(i, skip_faces)
+    if ~ismember(i, skip_faces)        
         p1 = ordered_triads(i*3-2, :)';
         p2 = ordered_triads(i*3-1, :)';
         p3 = ordered_triads(i*3-0, :)';
@@ -58,7 +55,17 @@ for i = 1:length(current_patch.Faces)
         facet_centroids(i, :) = mean(ordered_triads(i*3-2:i*3, :));
         facet_normals_to_incoming(i) = real(acos(max(min(dot(incoming_vector,facet_normals(i, :))/(norm(incoming_vector)*norm(facet_normals(i, :))),1),-1)));
         facet_force_contributions(i) = cos(pi - facet_normals_to_incoming(i)) * apparent_facet_areas(i);
+        
+        flattened_facet_centroids(i, :) = mean(flattened_ordered_triads(i*3-2:i*3-0, :));
+        facet_centroid_to_projection_plane(i, :) = ([planebasis1, planebasis2] * flattened_facet_centroids(i, :)')' - facet_centroids(i, :);
 %         disp(facet_force_contributions(i))
+    end
+end
+
+flattened_triangles = polyshape.empty;
+for i = 1:length(current_patch.Faces)
+    if ~ismember(i, skip_faces)
+        flattened_triangles(i) = polyshape(flattened_ordered_triads(i*3-2:i*3-0, 1), flattened_ordered_triads(i*3-2:i*3-0, 2));
     end
 end
 toc
@@ -66,48 +73,14 @@ toc
 figure
 hold on
 
-cmap = parula(1000);
-valued_variable = facet_force_contributions;
+cmap = parula(100);
 
+valued_variable = facet_centroid_to_projection_plane(boolean(repmat(incoming_vector', length(facet_centroid_to_projection_plane), 1)));
 for i = 1:length(current_patch.Faces)
     if ~ismember(i, skip_faces)
-        plot(flattened_triangles(i), 'FaceColor', cmap(ceil(valued_variable(i) / max(valued_variable) * length(cmap)), :))
+        lerp = (valued_variable(i) - min(valued_variable)) / (max(valued_variable) - min(valued_variable));
+        col = cmap((ceil(lerp * (length(cmap) - 2) * 0.95 + 1)), :);
+        plot(flattened_triangles(i), 'FaceColor', col)
+        scatter(flattened_facet_centroids(i, 1), flattened_facet_centroids(i, 2), [], col)
     end
 end
-
-
-
-
-% o = [100 s r];
-% d = [-100 s r];
-% 
-% valid_triads = struct('triads', [], 'dists', []);
-% triads = [];
-% dists = [];
-% 
-% tic
-% 
-% planebasis1 = rotx(pi / 2) * (d-o)';
-% planebasis2 = cross(planebasis1, (d-o)');
-% 
-% projected_vertices = current_patch.Vertices * [planebasis1, planebasis2];
-% 
-% for i = 1:length(current_patch.Faces)
-%     [flag, u, v, t, a] = fastRayTriangleIntersection(o, d, ordered_triads(i*3-2, :), ordered_triads(i*3-1, :), ordered_triads(i*3, :));
-% 
-%     if flag == 1
-%         valid_triads.triads = [valid_triads.triads; ordered_triads(i*3-2:i*3, :)];
-%         valid_triads.dists = [valid_triads.dists; t];
-%     end
-% end
-% toc
-% 
-% 
-% if size(valid_triads.triads)
-%     disp(valid_triads)
-%     [val, i] = min(valid_triads.dists);
-%     if length(valid_triads.dists) > 1
-%         scatter3(valid_triads.triads(i*3-2:i*3, 1), valid_triads.triads(i*3-2:i*3, 2), valid_triads.triads(i*3-2:i*3, 3), 'b.')
-%     end
-% % plot3([o(1) d(1)], [o(2) d(2)], [o(3) d(3)], 'r')
-% end
